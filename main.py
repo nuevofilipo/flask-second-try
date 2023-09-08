05cde2198068c1f81017cee14effc8d318e64845
nuevofilipo <111537061+nuevofilipo@users.noreply.github.com>
2023-09-08 21:51:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    return "hello world, luckily this application works!"
 
 
 if __name__ == '__main__':
